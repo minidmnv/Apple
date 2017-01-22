01908e9106f98758aeb75656d96a7d91d24194aa
Lidjan <minidmnv@gmail.com>
2017-01-22 01:50:22 +0100

    Add Bower, Grunt and simple angular js script

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.initConfig({
+        bower_concat: {
+            all: {
+                dest: 'src/main/resources/static/js/bower.js',
+                destCss: 'src/main/resources/static/css/bower.css'
+            }
+        },
+        uglify: {
+            build: {
+                src: 'src/main/resources/static/js/bower.js',
+                dest: 'src/main/resources/static/js/bower.min.js'
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-bower-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('build', [
+        'bower_concat',
+        'uglify'
+    ])
+};
+
---
+++ src/main/resources/static/js/apple.js
@@ -0,0 +1,4 @@
+angular.module('apple', [])
+    .controller('home', function($scope) {
+        $scope.greeting = {id: '112', content: 'Hello World!'}
+    })
