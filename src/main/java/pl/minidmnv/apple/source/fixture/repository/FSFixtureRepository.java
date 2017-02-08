87eff562df3c3a35e7c27d08704830d590ed4ec4
mnicinski <minidmnv@gmail.com>
2017-02-08 11:45:04 +0100

    Fix H2H document for fixture details fetching issues.

--- src/main/java/pl/minidmnv/apple/source/fixture/repository/FSFixtureRepository.java
+++ src/main/java/pl/minidmnv/apple/source/fixture/repository/FSFixtureRepository.java
@@ -1,6 +1,5 @@
 package pl.minidmnv.apple.source.fixture.repository;
 
-import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.Optional;
 
@@ -9,6 +8,7 @@ import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+
 import pl.minidmnv.apple.source.exception.FSConnectionException;
 
 /**
@@ -19,6 +19,7 @@ public class FSFixtureRepository implements FixtureRepository {
 
 	private static final Logger log = LoggerFactory.getLogger(FSFixtureRepository.class);
 	private static final String ROOT_URL = "http://www.flashscore.pl/";
+	private static final String ROOT_FEED_URL = "http://d.flashscore.pl/x/feed/";
 
 	@Override public Optional<Document> getUpcomingFixturesDocument() {
 		return getSiteDocument(ROOT_URL + "pilka-nozna/polska/ekstraklasa/spotkania/");
@@ -26,21 +27,24 @@ public class FSFixtureRepository implements FixtureRepository {
 
 	@Override
 	public Optional<Document> getFixtureHeadDetails(String fixtureId) {
-		return getSiteDocument(MessageFormat.format(ROOT_URL + "mecz/{0}/#h2h;overall", fixtureId));
+		return getSiteDocument(MessageFormat.format(ROOT_FEED_URL + "d_hh_{0}_pl_1", fixtureId));
 	}
 
 	private Optional<Document> getSiteDocument(String url) {
 		log.info("url: " + url);
-		Document siteDocument = null;
+		Document siteDocument;
 		try {
-			siteDocument = Jsoup.connect(url).get();
+			siteDocument = Jsoup.connect(url)
+					.referrer("http://d.flashscore.pl/x/feed/proxy-local")
+					.header("X-Fsign", "SW9D1eZo")
+					.get();
 		} catch (Exception e) {
 			log.error("Wystąpił błąd podczas połączenia FlashScore", e);
 			throw new FSConnectionException();
 		}
 
 		log.debug("Zwracam odpowiedź: " + siteDocument.outerHtml());
-		return Optional.ofNullable(siteDocument);
+		return Optional.of(siteDocument);
 	}
 
 }
